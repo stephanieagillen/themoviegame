2355caca1cdb6dad4b69c89dd2cc4790dfac6881
Stephanie Ross <stephanieagillen@gmail.com>
2018-10-17 08:03:35 -0600

    add games page

---
+++ app/Http/Controllers/GamesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GamesController extends Controller
+{
+    /**
+    *Display a listing of the games.
+    *
+    *@return Response
+    */
+    public function index(){
+
+    	return view('games.index');
+
+    }
+
+    /**
+    *Show the form for creating a new game.
+    *
+    *@return Response
+    */
+    public function create(){
+
+    }
+
+    public function show(Game $game){
+    	return $game;
+
+    	return view()
+    }
+}
---
+++ resources/views/games/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<h1>Hi There</h1>
+	<ul>
+		<li>Testing 1.2.3.</li>
+	</ul>
+
+@endsection
