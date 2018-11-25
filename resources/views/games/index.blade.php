1d099de297a5dba7627d535ea4c49ac537944919
Stephanie Ross <stephanieagillen@gmail.com>
2018-11-25 14:40:24 -0700

    update web.php

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,19 +1,11 @@
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Games</title>
-</head>
-<body>
+@extends('layouts.app')
  
+@section('content')
+
 <ul>
     @foreach($games as $game)
         <li><a href="/game/{{ $game->id }}">{{ $game->name }}</a></li>
     @endforeach
 </ul>
  
-</body>
-</html>
+@endsection
