124ba9092ebd337f209c7697a2880ae232ab7b39
Stephanie Ross <stephanieagillen@gmail.com>
2018-11-25 21:42:21 -0700

    update Games component

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -2,6 +2,7 @@
  
 @section('content')
 <example-component></example-component>
+<test></test>
 <ul>
     @foreach($games as $game)
         <li><a href="/game/{{ $game->id }}">{{ $game->name }}</a></li>
