0c12f5c86f740c2d84ebe075d99989d28e310350
Stephanie Ross <stephanieagillen@gmail.com>
2018-11-25 21:49:36 -0700

    update Games component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-<test></test>
+<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <button @click="initAddGame()" class="btn btn-primary btn-xs pull-right">
+                            + Add New Game
+                        </button>
+                        My Gamesss
+                    </div>
+ 
+                    <div class="panel-body">
+ 
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
+        <div class="modal fade" tabindex="-1" role="dialog" id="add_game_model">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Add New Game</h4>
+                    </div>
+                    <div class="modal-body">
+ 
+                        <div class="alert alert-danger" v-if="errors.length > 0">
+                            <ul>
+                                <li v-for="error in errors">{{ error }}</li>
+                            </ul>
+                        </div>
+ 
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" name="name" id="name" placeholder="Game Name" class="form-control"
+                                   v-model="game.name">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" @click="createGame" class="btn btn-primary">Submit</button>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+ 
+    </div>
+
+
+    
 <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -50,5 +101,48 @@
  
     </div>
 @endsection
+
+<script>
+    export default {
+        data() {
+            return {
+                game: {
+                    name: ''
+                },
+                errors: []
+            }
+        },
+        methods: {
+            initAddGame()
+            {
+                this.errors = [];
+                $("#add_game_model").modal("show");
+            },
+            createGame()
+            {
+                axios.post('/game', {
+                    name: this.game.name
+                })
+                    .then(response => {
+ 
+                        this.reset();
+ 
+                        $("#add_game_model").modal("hide");
+ 
+                    })
+                    .catch(error => {
+                        this.errors = [];
+                        if (error.response.data.errors.name) {
+                            this.errors.push(error.response.data.errors.name[0]);
+                        }
+                    });
+            },
+            reset()
+            {
+                this.game.name = '';
+            },
+        }
+    }
+</script>
 
 
