7f65b7cf00ab7c4423141599d6391afc47a09218
Stephanie Ross <stephanieagillen@gmail.com>
2018-11-26 07:53:21 -0700

    remove code from index.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,54 +47,6 @@
     </div>
 
 
-
-<div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <button @click="initAddGame()" class="btn btn-primary btn-xs pull-right">
-                            + Add New Game
-                        </button>
-                        My Games
-                    </div>
- 
-                    <div class="panel-body">
- 
-                    </div>
-                </div>
-            </div>
-        </div>
- 
-        <div class="modal fade" tabindex="-1" role="dialog" id="add_game_model">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                                aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title">Add New Game</h4>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="name">Name:</label>
-                            <input type="text" name="name" id="name" placeholder="Game Name" class="form-control"
-                                   v-model="game.name">
-                        </div>
-                        <div class="form-group">
-                            <label for="description">Description:</label>
-                            <textarea name="description" id="description" cols="30" rows="5" class="form-control"
-                                      placeholder="Game Description" v-model="game.description"></textarea>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" @click="createGame" class="btn btn-primary">Submit</button>
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
- 
-    </div>
 @endsection
 
 <script>
