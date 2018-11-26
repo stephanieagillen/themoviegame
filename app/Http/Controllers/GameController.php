d66ca6b26aca82b01790825889bd7a05d0ea4e35
Stephanie Ross <stephanieagillen@gmail.com>
2018-11-25 20:16:22 -0700

    update Games component

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -35,6 +35,86 @@ class GameController extends Controller
         return view('games.create');
     }
 
+ 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name'      => 'required|max:255'
+        ]);
+    
+        $game = Game::create([
+            'name'      => request('name'),
+            'user_id'   => Auth::user()->id
+        ]);
+
+        return response()->json([
+            'game'      => $game,
+            'message'   => 'Success'
+        ], 200);
+    }
+ 
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+ 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+ 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+ 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+
+
+
+    /**
+    *Dynamically create and play a new game
+    *
+    *@return Response
+    */
+    public function play(){
+
+    }
+
     /**
     *Show friendsgiving game
     *
@@ -44,6 +124,12 @@ class GameController extends Controller
         return Game::friendsgiving()
         ->where('id', '=', '1')
         ->get();
+    }
+
+
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
 
 }
