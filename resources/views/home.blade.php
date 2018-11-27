c3cf63e5d98c01b878bc8e708caafa0ff5195d8e
Stephanie Ross <stephanieagillen@gmail.com>
2018-11-26 19:40:04 -0700

    update home.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
- <script src="https://cdn.jsdelivr.net/npm/vue"></script>
-</head>
-<body>
+
   <div id="app">
   {{ message }}
 </div>
-</body>
-</html>
+
 
 <script>
 
