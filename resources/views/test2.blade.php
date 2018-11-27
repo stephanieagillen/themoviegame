98ec17299bfd3f4200bdcd8b23ee31b8beb0bf96
Stephanie Ross <stephanieagillen@gmail.com>
2018-11-26 21:16:41 -0700

    update home.php

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -5,24 +5,6 @@
 @section('content')
 <example-component></example-component>
 <games></games>
-<test></test>
-
- <div>
-  @{{ message }}
-</div>
-<div>
-test
-</div>
 
 @endsection
 
-
-<script>
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-});
-</script>
