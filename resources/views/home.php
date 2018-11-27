f2f26b1b31befb881579530a6c1ddf9fed2c97f1
Stephanie Ross <stephanieagillen@gmail.com>
2018-11-26 20:32:17 -0700

    update home.php

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,12 +1,11 @@
-
+ <script src="https://unpkg.com/vue@2.4.2"></script>
 <example-component></example-component>
 <div id="app">
   {{ message }}
 </div>
 <div>
-testing
+test
 </div>
-
 
 <script>
 
