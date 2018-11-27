935c0d1813b62b3d89bc8d5b7ca045fa356b4377
Stephanie Ross <stephanieagillen@gmail.com>
2018-11-26 19:28:53 -0700

    remove code from index.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,90 +1,24 @@
-
-@extends('layouts.app')
-
-@section('content')
-<div class="container" id="app">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <button @click="initAddGame()" class="btn btn-primary btn-xs pull-right">
-                            + Add New Game
-                        </button>
-                        My Games
-                    </div>
- 
-                    <div class="panel-body">
- 
-                    </div>
-                </div>
-            </div>
-        </div>
- 
-        <div class="modal fade" tabindex="-1" role="dialog" id="add_game_model">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                                aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title">Add New Game</h4>
-                    </div>
-                    <div class="modal-body">
- 
- 
-                        <div class="form-group">
-                            <label for="name">Name:</label>
-                            <input type="text" name="name" id="name" placeholder="Game Name" class="form-control"
-                                   v-model="game.name">
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" @click="createGame" class="btn btn-primary">Submit</button>
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
- 
-    </div>
-
-
-@endsection
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <div id="app">
+  <h1>{{ age * 2 }}</h1>
+</div>
+</body>
+</html>
 
 <script>
-    new Vue ({
-        el:'#app',
-        data: {
-                game: {
-                    name: ''
-                },
-                errors: []
-        },
-        methods: {
-            initAddGame()
-            {
-                this.errors = [];
-                $("#add_game_model").modal("show");
-            },
-            createGame()
-            {
-                axios.post('/game', {
-                    name: this.game.name
-                })
-                    .then(response => {
 
- 
-                        $("#add_game_model").modal("hide");
- 
-                    })
-                    .catch(error => {
-                        this.errors = [];
-                        if (error.response.data.errors.name) {
-                            this.errors.push(error.response.data.errors.name[0]);
-                        }
-                    });
-            },
-        }
-    });
+new Vue({
+  el: '#app',
+  data: {
+    age: 27,
+    name: 'Bo Andersen'
+  }
+});
 </script>
 
 
